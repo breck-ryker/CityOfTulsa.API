2e1802475f5219d81d63a453af480b84831df5df
Breck Ryker <breck.ryker@gmail.com>
2021-10-02 13:01:59 -0500

    edit

--- CityOfTulsa.API/Startup.cs
+++ CityOfTulsa.API/Startup.cs
@@ -38,7 +38,7 @@ namespace CityOfTulsa.API {
             );
 
          services.AddDbContext<DatabaseContext>(
-            options => options.UseSqlServer(Configuration.GetConnectionString("CityOfTulsa_AZURE"))
+            options => options.UseSqlServer(Configuration.GetConnectionString("CityOfTulsa"))
             );
 
          services.AddSwaggerGen(
